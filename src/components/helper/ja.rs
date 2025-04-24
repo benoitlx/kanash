874a358e97378448a10af33e2ec1c056051a1dd2
benoitlx <benoitleroux41@gmail.com>
2025-04-24 23:40:31 +0200

    feat: katakana

--- src/components/helper/ja.rs
+++ src/components/helper/ja.rs
@@ -1,8 +1,8 @@
 use rand::seq::IndexedRandom;
 use rand_pcg::Mcg128Xsl64;
 
-const KANA_NUMBER: usize = 71;
-const WANTED_KANA: [u16; KANA_NUMBER] = [
+const HIRAGANA_NUMBER: usize = 71;
+const WANTED_KANA: [u16; HIRAGANA_NUMBER] = [
     12354, 12356, 12358, 12360, 12362, 12363, 12364, 12365, 12366, 12367, 12368, 12369, 12370,
     12371, 12372, 12373, 12374, 12375, 12376, 12377, 12378, 12379, 12380, 12381, 12382, 12383,
     12384, 12385, 12386, 12388, 12389, 12390, 12391, 12392, 12393, 12394, 12395, 12396, 12397,
@@ -10,16 +10,25 @@ const WANTED_KANA: [u16; KANA_NUMBER] = [
     12411, 12412, 12413, 12414, 12415, 12416, 12417, 12418, 12420, 12422, 12424, 12425, 12426,
     12427, 12428, 12429, 12431, 12434, 12435,
 ];
+const KATAKANA_NUMBER: usize = 81;
+const WANTED_KATAKANA: [u16; KATAKANA_NUMBER] = [
+    12449, 12450, 12451, 12452, 12453, 12454, 12455, 12456, 12457, 12458, 12459, 12460, 12461,
+    12462, 12463, 12464, 12465, 12466, 12467, 12468, 12469, 12470, 12471, 12472, 12473, 12474,
+    12475, 12476, 12477, 12478, 12479, 12480, 12481, 12482, 12484, 12485, 12486, 12487, 12488,
+    12489, 12490, 12491, 12492, 12493, 12494, 12495, 12496, 12497, 12498, 12499, 12500, 12501,
+    12502, 12503, 12504, 12505, 12506, 12507, 12508, 12509, 12510, 12511, 12512, 12513, 12514,
+    12515, 12516, 12517, 12518, 12519, 12520, 12521, 12522, 12523, 12524, 12525, 12526, 12527,
+    12530, 12531, 12532,
+];
 
 fn random_hiragana(rng: &mut Mcg128Xsl64) -> String {
-    String::from_utf16(&[*WANTED_KANA.choose(rng).unwrap()]).expect("error")
+    String::from_utf16(&[*WANTED_KANA.choose(rng).unwrap()]).expect("error hiragana")
+}
+
+fn random_katakana(rng: &mut Mcg128Xsl64) -> String {
+    String::from_utf16(&[*WANTED_KATAKANA.choose(rng).unwrap()]).expect("error katakana")
 }
 
 pub fn random_kana(rng: &mut Mcg128Xsl64) -> String {
     random_hiragana(rng)
-}
-
-#[allow(dead_code)]
-pub fn get_hiragana() -> String {
-    String::from_utf16(&WANTED_KANA).unwrap()
 }
