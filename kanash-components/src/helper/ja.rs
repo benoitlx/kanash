15c14606b021c0b01b986c3f9fd714adb29c1691
benoitlx <benoitleroux41@gmail.com>
2025-11-28 11:49:11 +0100

    fix: remove pub before kana

--- kanash-components/src/helper/ja.rs
+++ kanash-components/src/helper/ja.rs
@@ -2,7 +2,7 @@ use rand::{seq::IndexedRandom, Rng};
 use rand_pcg::Mcg128Xsl64;
 
 const HIRAGANA_NUMBER: usize = 71;
-pub const WANTED_KANA: [u16; HIRAGANA_NUMBER] = [
+const WANTED_HIRAGANA: [u16; HIRAGANA_NUMBER] = [
     12354, 12356, 12358, 12360, 12362, 12363, 12364, 12365, 12366, 12367, 12368, 12369, 12370,
     12371, 12372, 12373, 12374, 12375, 12376, 12377, 12378, 12379, 12380, 12381, 12382, 12383,
     12384, 12385, 12386, 12388, 12389, 12390, 12391, 12392, 12393, 12394, 12395, 12396, 12397,
@@ -11,7 +11,7 @@ pub const WANTED_KANA: [u16; HIRAGANA_NUMBER] = [
     12427, 12428, 12429, 12431, 12434, 12435,
 ];
 const KATAKANA_NUMBER: usize = 81;
-pub const WANTED_KATAKANA: [u16; KATAKANA_NUMBER] = [
+const WANTED_KATAKANA: [u16; KATAKANA_NUMBER] = [
     12449, 12450, 12451, 12452, 12453, 12454, 12455, 12456, 12457, 12458, 12459, 12460, 12461,
     12462, 12463, 12464, 12465, 12466, 12467, 12468, 12469, 12470, 12471, 12472, 12473, 12474,
     12475, 12476, 12477, 12478, 12479, 12480, 12481, 12482, 12484, 12485, 12486, 12487, 12488,
@@ -22,7 +22,7 @@ pub const WANTED_KATAKANA: [u16; KATAKANA_NUMBER] = [
 ];
 
 pub fn random_hiragana(rng: &mut Mcg128Xsl64) -> String {
-    String::from_utf16(&[*WANTED_KANA.choose(rng).unwrap()]).expect("error hiragana")
+    String::from_utf16(&[*WANTED_HIRAGANA.choose(rng).unwrap()]).expect("error hiragana")
 }
 
 pub fn random_katakana(rng: &mut Mcg128Xsl64) -> String {
